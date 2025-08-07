66b27ce49f95b8a20db97db76a5957dffc8b7ddd
Amine-73 <aminchana.business@gmail.com>
2025-08-07 07:23:26 +0100

    create first Component for landingPage

---
+++ e-comweb/src/landingPage.js
@@ -0,0 +1,13 @@
+import Header from "./header"
+
+
+
+
+export default function landingPage(){
+    return (
+        <>
+            <Header/>
+            <h1>Hello we create A Web Site E-commerce Now</h1>
+        </>
+    )
+}
