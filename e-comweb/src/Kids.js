1f393b3875311103f1f9fb8ed284f5647d74b42f
Amine <aminechanna@gmail.com>
2025-08-14 01:27:41 +0100

    Create New File Kids

---
+++ e-comweb/src/Kids.js
@@ -0,0 +1,11 @@
+import Header from "./header"
+export default function Kids(){
+    return (
+        <>
+            <Header/>
+            <section>
+            <h1>Section Kids</h1>
+        </section>
+        </>
+    )
+}
