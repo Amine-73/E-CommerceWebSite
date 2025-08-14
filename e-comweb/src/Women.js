931d5cf582c4bc978b2dccd828bc7bffeff2aa54
Amine-73 <aminchana.business@gmail.com>
2025-08-14 01:29:13 +0100

    Create New Component Women

---
+++ e-comweb/src/Women.js
@@ -0,0 +1,12 @@
+import Header from "./header"
+
+export default function Women(){
+    return (
+        <>
+        <Header/>
+        <section>
+            <h1>Section Women</h1>
+        </section>
+        </>
+    )
+}
