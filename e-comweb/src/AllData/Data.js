066af4cb6dab5d5d29994b87605e967c7b9ac459
Amine <aminechanna@gmail.com>
2025-08-18 01:04:59 +0100

    create new file Data in folder AllData to creatContext have all Data Of my WEBSITE

---
+++ e-comweb/src/AllData/Data.js
@@ -0,0 +1,180 @@
+import { createContext } from "react"
+
+let Dataproduct=createContext(
+    {
+        Women:[
+    {
+        id:1,
+        name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+        image:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9igzce8KZlERMQ9NbVZIAvKIt8vEBjTdHAQ&s`,
+        lowPrice:"55.99$",
+        heightPrice:"190$"
+    },
+    {
+        id:2,
+        name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+        image:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLHkbMIsqBZW7fF8bKOHs9J9tt3rnHS09d2Q&s`,
+        lowPrice:"58.99$",
+        heightPrice:"180$"
+    },{
+        id:3,
+        name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+        image:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG-SxV7AafzYd6ieqog3z46yBZY-GiFr8KLg&s`,
+        lowPrice:"88.99$",
+        heightPrice:"280$"
+    },{
+        id:4,
+        name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+        image:`https://media.johnlewiscontent.com/i/JohnLewis/ww-block18-130625-v1?fmt=auto`,
+        lowPrice:"60.99$",
+        heightPrice:"200$"
+    },
+    ],
+        Children:[
+            {
+                id:'1',
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUvcflpTU2usyY1MyEIdAKRVY9aan5TLRUdQ&s`,
+                lowprice:"$77",
+                heightPrice:'$180'
+            },
+            {
+                id:'2',
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQSIFiZV4Ilmw2fYpNZapuE5p9GfyBiMidSw&s`,
+                lowprice:"$77",
+                heightPrice:'$180'
+            },
+            {
+                id:'3',
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi6INEBUZyDIAzV62RAEn-DwO4qSTbuYQbnw&s`,
+                lowprice:"$77",
+                heightPrice:'$180'
+            },
+            {
+                id:'4',
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://img.joomcdn.net/2ef8229072c4e45fe2eec7841730f07a82a30aa0_original.jpeg`,
+                lowprice:"$77",
+                heightPrice:'$180'
+            },
+            {
+                id:'5',
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/58/1279072/1.jpg?5578`,
+                lowprice:"$77",
+                heightPrice:'$180'
+            },
+            {
+                id:'6',
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://i.pinimg.com/236x/94/1e/b7/941eb773d4de289124497ece181f3d9d.jpg`,
+                lowprice:"$77",
+                heightPrice:'$180'
+            },
+            {
+                id:'7',
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9QFutiU9xa23VqhTo5zoe-b4Ie5ND2b9-CqF761_RrDe94YcDL7SaW_VYI5qwuNFQOAY&usqp=CAU`,
+                lowprice:"$77",
+                heightPrice:'$180'
+            },
+            {
+                id:'8',
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRotpjgfwJc8fw7E51ZPn_YfYyETN5xHujgaw&s`,
+                lowprice:"$77",
+                heightPrice:'$180'
+            },
+        ],
+        AllDataWomen:[
+            {
+                id:1,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://mikadopersonalstyling.com/wp-content/uploads/2020/06/womens-fashion-tips.jpg`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+            {
+                id:2,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://officesalt.com/wp-content/uploads/2018/05/12TRENDY-WORK-OUTFITS-FOR-BUSINESS-WOMEN.jpg`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+            {
+                id:3,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://i.pinimg.com/474x/42/8b/d5/428bd53aed20b0c333c9812b1ddb9570.jpg`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+            {
+                id:4,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://adrift.com.au/cdn/shop/articles/unnamed_11_d98e1778-a673-4321-8f9b-2651cadd3ad8.jpg?v=1753683130`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+            {
+                id:5,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://media.johnlewiscontent.com/i/JohnLewis/ww-block18-130625-v1?fmt=auto`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+            {
+                id:6,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG-SxV7AafzYd6ieqog3z46yBZY-GiFr8KLg&s`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+            {
+                id:7,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLHkbMIsqBZW7fF8bKOHs9J9tt3rnHS09d2Q&s`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+            {
+                id:8,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9igzce8KZlERMQ9NbVZIAvKIt8vEBjTdHAQ&s`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+            {
+                id:9,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://stylishlyme.com/wp-content/uploads/2015/09/Business-Casual-for-Women-Clothing-Guide.jpg`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+            {
+                id:10,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://i.pinimg.com/474x/c3/be/29/c3be29cbf7416d1b8eced0ba4392593d.jpg`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+            {
+                id:11,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://i.pinimg.com/236x/03/04/7b/03047bdd07c3f9d2949609014f92eead.jpg`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+            {
+                id:12,
+                name:"Striped Flutter Sleeve Overtop Collor Peplum Hem Blouse",
+                image:`https://i.pinimg.com/236x/66/7d/4c/667d4ceb07368a4fa84496ec05113c16.jpg`,
+                lowPrice:"55.99$",
+                heightPrice:"190$"
+            },
+        ]
+    }
+
+)
+export default  Dataproduct;
